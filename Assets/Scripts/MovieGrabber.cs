959e4c20131959f9c2aa4d883b28366094a1ff37
Zachary Smith <128327411+HydeZero@users.noreply.github.com>
2025-01-01 16:03:50 -0600

    Finish basic functionality and fix compile error

---
+++ Assets/Scripts/MovieGrabber.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MovieGrabber : MonoBehaviour
+{
+    public static MovieGrabber Instance;
+
+    public string moviePath; // The path to the movie file
+
+    private void Awake()
+    {
+        // Make sure the MovieGrabber always stays even when moving scenes. We need this to memorize the file path and play the movie when we enter VR mode.
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this; // this line crashes and I don't know why
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void SavePathAndSwitchScene(string path)
+    {
+        moviePath = path;
+        SceneManager.LoadScene("VR-Theater"); // Switch to the VR scene after movie has been selected
+    }
+
+    // Ask the user to select an MP4 file to play
+    public void AskForFile()
+    {
+        NativeFilePicker.PickFile(SavePathAndSwitchScene, "movie/mp4");
+    }
+}
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    private string url;
+
+    private VideoPlayer videoPlayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        try
+        {
+            videoPlayer.source = VideoSource.Url;
+        }
+        catch
+        {
+            Debug.Log("VideoPlayer source was unable to be set to URL");
+            Application.Quit(); // Exit the application if the video player source cannot be set
+        }
+        try // try to set the video player URL to the selected movie
+        {
+            url = "file://" + MovieGrabber.Instance.moviePath;
+            videoPlayer.url = url;
+        }
+        catch
+        {
+            Debug.Log("VideoPlayer URL was unable to be set to the movie path");
+            Application.Quit(); // Exit the application if the video player URL cannot be set
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Here we expose all of the basic video player functions to outside scripts
+    public void Play()
+    {
+        videoPlayer.Play();
+    }
+
+    public void Pause()
+    {
+        videoPlayer.Pause();
+    }
+
+    public void Restart()
+    {
+        videoPlayer.Stop();
+        videoPlayer.Play();
+    }
+
+    public void Stop()
+    {
+        videoPlayer.Stop();
+    }
+
+    public void Rewind()
+    {
+        videoPlayer.time -= 10;
+    }
+
+    public void Forward()
+    {
+        videoPlayer.time += 10;
+    }
+
+    public void ExitMovie()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
